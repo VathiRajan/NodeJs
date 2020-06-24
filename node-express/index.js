4395c81b4279eae38417cdddff8bb9309cdfb5fb
vathirajan <vathirajanv95@gmail.com>
2020-06-24 15:51:16 +0530

    initial-commit

--- node-express/index.js
+++ node-express/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const http = require('http');
-
+const morgan = require('morgan');
 const hostname = 'localhost'
 const port = 3000;
 const app = express();
+app.use(morgan('dev'));//Morgan is used for logs
+app.use(express.static(__dirname + '/public')); //used for serving stating html pages 
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
